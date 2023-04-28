b2ddb38982f30f62e26e7e5b76ddd370d7a4570f
Bestimran <imrantahir9918@gmail.com>
2023-04-29 03:20:52 +0600

    add user done

--- src/Component/AddUser.jsx
+++ src/Component/AddUser.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import { Button, FormControl, FormGroup, Input, InputLabel, Typography, styled} from '@mui/material';
+import React, { useState } from 'react';
+
+const  Container = styled(FormGroup)`
+   width:40%;
+   margin: 5% auto 0 auto;
+   & > div {
+    margin-top: 20px;
+   }
+`;
+
+const userDetails = {
+    name:'',
+    username:'',
+    email:'',
+    phone:'',
+}
 
 const AddUser = () => {
+const [user, setUser] = useState({userDetails});
+
+
+    const changeValue = (e)=>{
+        setUser({...user,[e.target.name]:e.target.value});
+    };
+
+    const addUser = ()=>{
+        console.log('yes')
+    }
+
     return (
-        <div>
-            <h1>this is add user page</h1>
-        </div>
+        <Container>
+            <Typography variant='h4'>Add User</Typography>
+            <FormControl>
+                <InputLabel>Enter your Name</InputLabel>
+                <Input onChange={(e)=> changeValue(e)} name='name' />
+            </FormControl>
+            <FormControl>
+                <InputLabel>Enter your UserName</InputLabel>
+                <Input onChange={(e)=> changeValue(e)} name='username'/>
+            </FormControl>
+            <FormControl>
+                <InputLabel>Enter your Email</InputLabel>
+                <Input onChange={(e)=> changeValue(e)} name='email'/>
+            </FormControl>
+            <FormControl>
+                <InputLabel>Enter your Phone</InputLabel>
+                <Input onChange={(e)=> changeValue(e)} name='phone'/>
+            </FormControl>
+            <FormControl>
+                <Button onClick={()=> addUser()} variant='contained'>Add User</Button>
+            </FormControl>
+        </Container>
     );
 };
 
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,18 +1,22 @@
 import { AppBar, Toolbar, styled} from '@mui/material';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Tab = styled('p')`
+const Tab = styled(NavLink)`
      font-size:20px;
-     margin-right:20px
+     margin-right:20px;
+     color:inherit;
+     text-decoration:none;
+     font-weight:bold;
 `;
 
 const Navbar = () => {
     return (
         <AppBar position='static'>
             <Toolbar>
-                <Tab>Crud Operation</Tab>
-                <Tab>All Users</Tab>
-                <Tab>Add User</Tab>
+                <Tab to='/'>Crud Operation</Tab>
+                <Tab to='allUsers'>All Users</Tab>
+                <Tab to='addUser'>Add User</Tab>
             </Toolbar>
         </AppBar>
     );
